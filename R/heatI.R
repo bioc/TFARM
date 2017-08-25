1c5e50508fbe2437cb5a1ec6b0a742bed84be985
gaiac <gaia.ceddia@gmail.com>
2017-08-25 13:50:10 +0200

    Update heatI.R

--- R/heatI.R
+++ R/heatI.R
@@ -24,45 +24,40 @@
 #' # and their combinations in two elements:
 #' heatI(p, I_c_2)
 
-heatI <- function(TFs,I){
+heatI <- function(TFs, I) {
     l <- length(TFs)
-    estract<- function(x)
-    {
-    vec <- rep(0, length(TFs))
-    for (i in 1:length(vec))
-    {
-    vec[i] <- gregexpr(TFs[i], x)[[1]][1] != -1
+    estract <- function(x) {
+        vec <- rep(0, length(TFs))
+        for (i in 1:length(vec)) {
+            vec[i] <- gregexpr(TFs[i], x)[[1]][1] != -1
+        }
+        return(vec)
     }
-    return (vec)
-  }
-  aa = t(sapply(I[,1],estract))
-  matrix_imp <- matrix(0, l, l)
-  for ( i in 1:dim(aa)[1])
-  {
-    pos = rep(NA,2)
-    pos[1] <- which(aa[i,] == 1)[1]
-    pos[2] <- which(aa[i,] == 1)[2]
-    if (is.na(pos[2]))
-    {
-      matrix_imp[pos[1], pos[1]] = I[i,]$imp
+    aa = t(sapply(I[, 1], estract))
+    matrix_imp <- matrix(0, l, l)
+    for (i in 1:dim(aa)[1]) {
+        pos = rep(NA, 2)
+        pos[1] <- which(aa[i, ] == 1)[1]
+        pos[2] <- which(aa[i, ] == 1)[2]
+        if (is.na(pos[2])) {
+            matrix_imp[pos[1], pos[1]] = I[i, ]$imp
+        }
+        if (!is.na(pos[2])) {
+            matrix_imp[pos[1], pos[2]] = I[i, ]$imp
+            matrix_imp[pos[2], pos[1]] = I[i, ]$imp
+        }
     }
-    if (!is.na(pos[2]))
-    {
-      matrix_imp[pos[1], pos[2]]  = I[i,]$imp
-      matrix_imp[pos[2], pos[1]] = I[i,]$imp
-    }
-  }
-  rownames(matrix_imp) = TFs
-  colnames(matrix_imp) = TFs
-  par(mar = c(7,7,4,10))
-  image(matrix_imp, axes=FALSE,xlab='', ylab='',col=heat.colors(20),
-        ylim=c(1.04,-0.04))
-  axis(1, at=seq(0,1, length=l), labels=rownames(matrix_imp), las=2,
-       cex.axis=0.8)
-  axis(2, at=seq(0,1, length=l), labels=rownames(matrix_imp), las=2,
-       cex.axis=0.8)
-  im<-image.plot(matrix_imp,col=heat.colors(20),legend.only=TRUE,
-                 ylim=c(1.04,-0.04))
-  title(main='Importance index of pairs of Transcription Factors')
-  return(im)
+    rownames(matrix_imp) = TFs
+    colnames(matrix_imp) = TFs
+    par(mar = c(7, 7, 4, 10))
+    image(matrix_imp, axes = FALSE, xlab = "", ylab = "", col = heat.colors(20), 
+        ylim = c(1.04, -0.04))
+    axis(1, at = seq(0, 1, length = l), labels = rownames(matrix_imp), 
+        las = 2, cex.axis = 0.8)
+    axis(2, at = seq(0, 1, length = l), labels = rownames(matrix_imp), 
+        las = 2, cex.axis = 0.8)
+    im <- image.plot(matrix_imp, col = heat.colors(20), legend.only = TRUE, 
+        ylim = c(1.04, -0.04))
+    title(main = "Importance index of pairs of Transcription Factors")
+    return(im)
 }
